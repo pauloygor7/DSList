548ee7b418bcb38f4ee8be8d19a28158672cb427
Paulo Ygor Feitoza <pauloygor700@gmail.com>
2025-05-20 18:42:40 -0300

    Game seed

---
+++ dslist/src/main/resources/data.sql
@@ -0,0 +1,219 @@
+INSERT INTO
+    tb_game (
+        genre,
+        img_url,
+        long_description,
+        platforms,
+        score,
+        short_description,
+        title,
+        game_year
+    )
+VALUES (
+        'Role-playing (RPG), Shooter',
+        'https://raw.githubusercontent.com/devsuperior/java-spring-dslist/main/resources/1.png',
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus dolorum illum placeat eligendi, quis maiores veniam. Incidunt dolorum, nisi deleniti dicta odit voluptatem nam provident temporibus reprehenderit blanditiis consectetur tenetur. Dignissimos blanditiis quod corporis iste, aliquid perspiciatis architecto quasi tempore ipsam voluptates ea ad distinctio, sapiente qui, amet quidem culpa.',
+        'XBox, Playstation, PC',
+        4.8,
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit esse officiis corrupti unde repellat non quibusdam! Id nihil itaque ipsum!',
+        'Mass Effect Trilogy',
+        2012
+    );
+
+INSERT INTO
+    tb_game (
+        genre,
+        img_url,
+        long_description,
+        platforms,
+        score,
+        short_description,
+        title,
+        game_year
+    )
+VALUES (
+        'Role-playing (RPG), Adventure',
+        'https://raw.githubusercontent.com/devsuperior/java-spring-dslist/main/resources/2.png',
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus dolorum illum placeat eligendi, quis maiores veniam. Incidunt dolorum, nisi deleniti dicta odit voluptatem nam provident temporibus reprehenderit blanditiis consectetur tenetur. Dignissimos blanditiis quod corporis iste, aliquid perspiciatis architecto quasi tempore ipsam voluptates ea ad distinctio, sapiente qui, amet quidem culpa.',
+        'XBox, Playstation, PC',
+        4.7,
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit esse officiis corrupti unde repellat non quibusdam! Id nihil itaque ipsum!',
+        'Red Dead Redemption 2',
+        2018
+    );
+
+INSERT INTO
+    tb_game (
+        genre,
+        img_url,
+        long_description,
+        platforms,
+        score,
+        short_description,
+        title,
+        game_year
+    )
+VALUES (
+        'Role-playing (RPG), Adventure',
+        'https://raw.githubusercontent.com/devsuperior/java-spring-dslist/main/resources/3.png',
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus dolorum illum placeat eligendi, quis maiores veniam. Incidunt dolorum, nisi deleniti dicta odit voluptatem nam provident temporibus reprehenderit blanditiis consectetur tenetur. Dignissimos blanditiis quod corporis iste, aliquid perspiciatis architecto quasi tempore ipsam voluptates ea ad distinctio, sapiente qui, amet quidem culpa.',
+        'XBox, Playstation, PC',
+        4.7,
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit esse officiis corrupti unde repellat non quibusdam! Id nihil itaque ipsum!',
+        'The Witcher 3: Wild Hunt',
+        2014
+    );
+
+INSERT INTO
+    tb_game (
+        genre,
+        img_url,
+        long_description,
+        platforms,
+        score,
+        short_description,
+        title,
+        game_year
+    )
+VALUES (
+        'Role-playing (RPG), Adventure',
+        'https://raw.githubusercontent.com/devsuperior/java-spring-dslist/main/resources/4.png',
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus dolorum illum placeat eligendi, quis maiores veniam. Incidunt dolorum, nisi deleniti dicta odit voluptatem nam provident temporibus reprehenderit blanditiis consectetur tenetur. Dignissimos blanditiis quod corporis iste, aliquid perspiciatis architecto quasi tempore ipsam voluptates ea ad distinctio, sapiente qui, amet quidem culpa.',
+        'XBox, Playstation, PC',
+        3.8,
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit esse officiis corrupti unde repellat non quibusdam! Id nihil itaque ipsum!',
+        'Sekiro: Shadows Die Twice',
+        2019
+    );
+
+INSERT INTO
+    tb_game (
+        genre,
+        img_url,
+        long_description,
+        platforms,
+        score,
+        short_description,
+        title,
+        game_year
+    )
+VALUES (
+        'Role-playing (RPG), Adventure',
+        'https://raw.githubusercontent.com/devsuperior/java-spring-dslist/main/resources/5.png',
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus dolorum illum placeat eligendi, quis maiores veniam. Incidunt dolorum, nisi deleniti dicta odit voluptatem nam provident temporibus reprehenderit blanditiis consectetur tenetur. Dignissimos blanditiis quod corporis iste, aliquid perspiciatis architecto quasi tempore ipsam voluptates ea ad distinctio, sapiente qui, amet quidem culpa.',
+        'XBox, Playstation, PC',
+        4.6,
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit esse officiis corrupti unde repellat non quibusdam! Id nihil itaque ipsum!',
+        'Ghost of Tsushima',
+        2012
+    );
+
+INSERT INTO
+    tb_game (
+        genre,
+        img_url,
+        long_description,
+        platforms,
+        score,
+        short_description,
+        title,
+        game_year
+    )
+VALUES (
+        'Platform',
+        'https://raw.githubusercontent.com/devsuperior/java-spring-dslist/main/resources/6.png',
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus dolorum illum placeat eligendi, quis maiores veniam. Incidunt dolorum, nisi deleniti dicta odit voluptatem nam provident temporibus reprehenderit blanditiis consectetur tenetur. Dignissimos blanditiis quod corporis iste, aliquid perspiciatis architecto quasi tempore ipsam voluptates ea ad distinctio, sapiente qui, amet quidem culpa.',
+        'Super Ness, PC',
+        4.7,
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit esse officiis corrupti unde repellat non quibusdam! Id nihil itaque ipsum!',
+        'Super Mario World',
+        1990
+    );
+
+INSERT INTO
+    tb_game (
+        genre,
+        img_url,
+        long_description,
+        platforms,
+        score,
+        short_description,
+        title,
+        game_year
+    )
+VALUES (
+        'Platform',
+        'https://raw.githubusercontent.com/devsuperior/java-spring-dslist/main/resources/7.png',
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus dolorum illum placeat eligendi, quis maiores veniam. Incidunt dolorum, nisi deleniti dicta odit voluptatem nam provident temporibus reprehenderit blanditiis consectetur tenetur. Dignissimos blanditiis quod corporis iste, aliquid perspiciatis architecto quasi tempore ipsam voluptates ea ad distinctio, sapiente qui, amet quidem culpa.',
+        'XBox, Playstation, PC',
+        4.6,
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit esse officiis corrupti unde repellat non quibusdam! Id nihil itaque ipsum!',
+        'Hollow Knight',
+        2017
+    );
+
+INSERT INTO
+    tb_game (
+        genre,
+        img_url,
+        long_description,
+        platforms,
+        score,
+        short_description,
+        title,
+        game_year
+    )
+VALUES (
+        'Platform',
+        'https://raw.githubusercontent.com/devsuperior/java-spring-dslist/main/resources/8.png',
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus dolorum illum placeat eligendi, quis maiores veniam. Incidunt dolorum, nisi deleniti dicta odit voluptatem nam provident temporibus reprehenderit blanditiis consectetur tenetur. Dignissimos blanditiis quod corporis iste, aliquid perspiciatis architecto quasi tempore ipsam voluptates ea ad distinctio, sapiente qui, amet quidem culpa.',
+        'XBox, Playstation, PC',
+        4,
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit esse officiis corrupti unde repellat non quibusdam! Id nihil itaque ipsum!',
+        'Ori and the Blind Forest',
+        2015
+    );
+
+INSERT INTO
+    tb_game (
+        genre,
+        img_url,
+        long_description,
+        platforms,
+        score,
+        short_description,
+        title,
+        game_year
+    )
+VALUES (
+        'Platform',
+        'https://raw.githubusercontent.com/devsuperior/java-spring-dslist/main/resources/9.png',
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus dolorum illum placeat eligendi, quis maiores veniam. Incidunt dolorum, nisi deleniti dicta odit voluptatem nam provident temporibus reprehenderit blanditiis consectetur tenetur. Dignissimos blanditiis quod corporis iste, aliquid perspiciatis architecto quasi tempore ipsam voluptates ea ad distinctio, sapiente qui, amet quidem culpa.',
+        'XBox, Playstation, PC',
+        4.6,
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit esse officiis corrupti unde repellat non quibusdam! Id nihil itaque ipsum!',
+        'Cuphead',
+        2017
+    );
+
+INSERT INTO
+    tb_game (
+        genre,
+        img_url,
+        long_description,
+        platforms,
+        score,
+        short_description,
+        title,
+        game_year
+    )
+VALUES (
+        'Platform',
+        'https://raw.githubusercontent.com/devsuperior/java-spring-dslist/main/resources/10.png',
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus dolorum illum placeat eligendi, quis maiores veniam. Incidunt dolorum, nisi deleniti dicta odit voluptatem nam provident temporibus reprehenderit blanditiis consectetur tenetur. Dignissimos blanditiis quod corporis iste, aliquid perspiciatis architecto quasi tempore ipsam voluptates ea ad distinctio, sapiente qui, amet quidem culpa.',
+        'XBox, Playstation, PC',
+        4.9,
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit esse officiis corrupti unde repellat non quibusdam! Id nihil itaque ipsum!',
+        'GTA 5',
+        2019
+    );
