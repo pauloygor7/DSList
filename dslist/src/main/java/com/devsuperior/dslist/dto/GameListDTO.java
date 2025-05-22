b2314f91a8eed30b3fd5be3a7ce6bdd97096e895
Paulo Ygor Feitoza <pauloygor700@gmail.com>
2025-05-21 22:16:51 -0300

    GET lists

---
+++ dslist/src/main/java/com/devsuperior/dslist/dto/GameListDTO.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.GameList;
+
+public class GameListDTO {
+    private Long id;
+    private String nome;
+
+    public GameListDTO() {
+    }
+
+    public GameListDTO(GameList entity) {
+        id = entity.getId();
+        nome = entity.getNome();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
---
+++ dslist/src/main/java/com/devsuperior/dslist/service/GameListService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dslist.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.repository.GameListRepository;
+
+@Service
+public class GameListService {
+
+    @Autowired
+    private GameListRepository glr;
+
+    public List<GameListDTO> findAll() {
+        List<GameList> result = glr.findAll();
+        List<GameListDTO> listdto = result.stream().map(x -> new GameListDTO(x)).toList();
+        return listdto;
+    }
+}
